66a87a1f47c62015af00c24a03c88b9f4553e4ad
Pavel Zaikin <zaikinpv@gmail.com>
2014-10-08 23:51:22 +0600

    get all articles
    Change:  sql_UPDATE query, for loaded articles, and change file
    extension.

--- get_articles.py
+++ get_articles.py
@@ -3,7 +3,8 @@ from scanner import *
 from md5calc import *
 import gzip,os
 
-sql = "SELECT Link,MD5 From articles WHERE  Loaded=0 and Link = '/doi/abs/10.1137/1102023'"
+sql = "SELECT Link,MD5 From articles WHERE  Loaded=0"
+sql_u = "UPDATE articles SET Loaded = 1 WHERE Link = {link}"
 
 cur.execute(sql)
 for a in cur.fetchall():
@@ -13,8 +14,9 @@ for a in cur.fetchall():
 	b = r.read()
 	if not os.path.exists(md5hex.byte1+"\\"+md5hex.byte2):
 		os.makedirs(md5hex.byte1+"\\"+md5hex.byte2)
-	f = gzip.open(md5hex.byte1+"\\"+md5hex.byte2+"\\"+md5hex.md5hex+".html.gzip","wb")
-	f.write(b)
+	f = gzip.open(md5hex.byte1+"\\"+md5hex.byte2+"\\"+md5hex.md5hex+".html.gz","wb")
+	f.write(b.strip())
 	f.close()
+	cur.execute(sql_u.format(link=a[0]))
 
 
